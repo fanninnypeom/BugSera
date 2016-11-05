71ca581fe199e26ae554fa65d29c4e3f96f8029b
fanninnypeom <976961340@qq.com>
2016-11-05 14:17:04 +0000

    去掉了无用代码

--- insertAction/logoutAction.php
+++ insertAction/logoutAction.php
@@ -1,5 +1,5 @@
 <?php
-session_start();//在某一个页面使用session之前一定要先用这个函数，否则对session做出的更改都会被丢掉，浪费我好久时间
+session_start();//在某一个页面使用session之前一定要先用这个函数，否则对session做出的更改都会被丢掉，这里浪费我好久时间
 $_SESSION["login"]=0;
 //echo $_SESSION["login"];
 header("Location: http://127.0.0.1");
--- insertAction/registerAction.php
+++ insertAction/registerAction.php
@@ -10,23 +10,7 @@ if (!$con)
 mysql_select_db("BugFade", $con);
 
 $result = mysql_query("select * from total where name='users'") or die(mysql_error());//得到已有用户的数量
-//print_r(mysql_fetch_array($result));
-//$r=mysql_fetch_array($result,MYSQL_NUM);
-//$re=array();
-//$rowCount=0;
-//echo "length1";
-//while(
 	$row = mysql_fetch_array($result);//这个函数第一次被调用可以得到数据，再调用就没数据了，注意一下
-//	)
-//  {
-//  print_r(mysql_fetch_array($result));
-//    echo $row;
-//  	echo "LLength";
-//  	echo $row[1];
-//  $re[0]=$row;
-//  print_r(mysql_fetch_array($row));
-//  }
-//$re[0]=mysql_fetch_assoc($result);
 $rowCount=$row['length'];
 $rowCount=$rowCount+1;
 //echo $rowCount;
@@ -41,16 +25,6 @@ else{
 	WHERE name='users'";	
 }
 mysql_query($sql,$con);
-//echo 'row';
-//echo $rowCount;
-
-
-//echo $_POST;
-
-//foreach($_POST as $key=>$value)
-//echo $key."=>".$value;
-// print_r($_POST);
-
 
 $sql="INSERT INTO users (ID, Name, Password,Email,Reputation)
 VALUES
